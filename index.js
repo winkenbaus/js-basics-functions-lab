039a5897edc7752529818d64c20f4d94a3ae9fa6
caitlin <caitlin.wink@gmail.com>
2021-03-19 13:40:39 -0700

    commit tree 0c058dcc6fb609e7dc89416634a4365aeb64451e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue-42)
+}
+
+function distanceFromHqInFeet(someValue){
+    const blocks = distanceFromHqInBlocks(someValue)
+    return blocks*264
+}
+
+
+function distanceTraveledInFeet(){
+
+}
+
+function calculatesFarePrice(){
+
+}
